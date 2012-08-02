e15bfeffdef780e398faf2b8822b138507821581
Stefan heimes <stefan_heimes@hotmail.com>
2012-08-02 14:39:32 +0200

    Update language

--- system/modules/realurl/languages/de/default.php
+++ system/modules/realurl/languages/de/default.php
@@ -33,4 +33,5 @@ $GLOBALS['TL_LANG']['ERR']['folderurl']         = 'Das Wort "%s" kann nicht im A
 $GLOBALS['TL_LANG']['ERR']['aliasExistsFolder'] = 'Der Alias "%s" existiert bereits! (Der übergeordnete Alias wurde automatisch hinzugefügt)';
 $GLOBALS['TL_LANG']['ERR']['noRootPageFound']   = 'Konnte keine Rootpage finden.';
 $GLOBALS['TL_LANG']['ERR']['autoItemEnabeld']   = 'Die Einstellung "auto_item-Parameter" kann nicht mit Real URL verwendet werden.';
+$GLOBALS['TL_LANG']['ERR']['realUrlKeywords']   = 'Der Alias ist als Keyword reserviert.';
 
